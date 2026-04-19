45954bea95e610af89ad702a7891fe02af0492b4
Faudzan10 <irdhan.faudzan@gmail.com>
2026-04-19 18:08:52 +0800

    Update index.php

--- views/index.php
+++ views/index.php
@@ -22,7 +22,7 @@ $data_menu   = mysqli_query($conn, "SELECT * FROM menu ORDER BY id DESC");
     <title>Teras Alam Ulin</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/style.css?v=5">
+    <link rel="stylesheet" href="assets/css/style.css?v=6">
 </head>
 <body>
 
